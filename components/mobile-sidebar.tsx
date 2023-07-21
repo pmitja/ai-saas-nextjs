ae997f0a40600acca008350ca597ff7efa26a09c
Mitja Pak <pmitja1@gmail.com>
2023-07-21 23:10:57 +0200

    Added the support for mobile sidebar.

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import SideBar from "@/components/sidebar";
+
+const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button className="md:hidden" variant={"ghost"} size={"icon"}>
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <SideBar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSidebar;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,10 @@
-import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
-import { Menu } from "lucide-react";
+import MobileSidebar from "@/components/mobile-sidebar";
 
 const Navbar = () => {
   return (
     <div className="flex items-center p-4">
-      <Button className="md:hidden" variant={"ghost"} size={"icon"}>
-        <Menu />
-      </Button>
+      <MobileSidebar />
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
       </div>
